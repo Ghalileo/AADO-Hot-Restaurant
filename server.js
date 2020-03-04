9b6c0f4bb37dfc07224796cb19f85226ef5cbc3f
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-03-04 15:02:35 -0600

    Wroking reservations.

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const reservations = [];
 app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -16,18 +19,21 @@ app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserveAdd.html"));
 });
 
-// app.get("/api/tables", function(req, res) {
-//     return res.json(reservations);
-// });
+app.get("/api/reservations", function(req, res) {
+    return res.json(reservations);
+});
 
-app.post("/reserve", function(req, res) {
+app.post("/api/reservations", function(req, res) {
     var newReservation = req.body;
 
     newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
 
-    reservations.push(newReservation);
-});
+    console.log(newReservation);
 
+    reservations.push(newReservation);
+
+    res.json(newReservation);
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
