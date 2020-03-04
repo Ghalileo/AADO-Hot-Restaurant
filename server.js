e2df71971293cd947c2a0e7bb7318cc3974b21e8
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-03-04 14:34:45 -0600

    reserve page-up

--- server.js
+++ server.js
@@ -3,10 +3,33 @@
 const express = require("express");
 const path = require("path");
 
+const reservations = [];
+
 app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
-})
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserveAdd.html"));
+});
+
+// app.get("/api/tables", function(req, res) {
+//     return res.json(reservations);
+// });
+
+app.post("/reserve", function(req, res) {
+    var newReservation = req.body;
+
+    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+    reservations.push(newReservation);
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
 
