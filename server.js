29bb92b948b56b67d79009d432ff0d7943cf5e48
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-03-04 13:45:55 -0600

    update server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+
+
+const express = require("express");
+const path = require("path");
+
+app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
